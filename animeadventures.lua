3cd61248b39239fb161c9ff78f54506d27e14e2a
KeremHuba <125777677+KeremHuba@users.noreply.github.com>
2023-02-26 19:29:51 +0300

    Update animeadventures.lua

--- animeadventures.lua
+++ animeadventures.lua
@@ -1175,7 +1175,7 @@ autoclngtab:CreateToggle({
         
 --#region Auto Farm Tab
 
-autofarmset:CreateButton({
+autofarmtab:CreateButton({
     Name = "Set Unit 1 Posit"; 
     Callback = function()
         warn(1)
@@ -1184,7 +1184,7 @@ autofarmset:CreateButton({
     end
 })
 
-autofarmset:CreateButton({
+autofarmtab:CreateButton({
     Name = "Set Unit 2 Posit"; 
     Callback = function()
         warn(1)
@@ -1193,7 +1193,7 @@ autofarmset:CreateButton({
     end
 })
 
-autofarmset:CreateButton({
+autofarmtab:CreateButton({
     Name = "Set Unit 3 Posit"; 
     Callback = function()
         warn(1)
@@ -1202,7 +1202,7 @@ autofarmset:CreateButton({
     end
 })
 
-autofarmset:CreateButton({
+autofarmtab:CreateButton({
     Name = "Set Unit 4 Posit"; 
     Callback = function()
         warn(1)
@@ -1215,7 +1215,7 @@ autofarmset:CreateButton({
 local axxc = game.Players.LocalPlayer.PlayerGui["spawn_units"].Lives.Main.Desc.Level.Text:split(" ")
 
 if tonumber(axxc[2]) >= 20 then
-    autofarmset:CreateButton({
+    autofarmtab:CreateButton({
         Name = "Set Unit 5 Posit"; 
         Callback = function()
             warn(1)
@@ -1226,7 +1226,7 @@ if tonumber(axxc[2]) >= 20 then
 end
 
 if tonumber(axxc[2]) >= 50 then
-    autofarmset:CreateButton({
+    autofarmtab:CreateButton({
         Name = "Set Unit 6 Posit"; 
         Callback = function()
             warn(1)
